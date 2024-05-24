8585293e5870db583bd82892e445cb277cc694eb
Pasang Lama <pasangtheeng14@gmail.com>
2024-05-24 10:50:37 +0545

    Home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaList } from "react-icons/fa6";
+import { FiGrid } from "react-icons/fi";
+import { IoCheckmarkOutline } from "react-icons/io5";
+import EmptyDrive from "../assets/Illustrations/EmptyDrive";
 
 function Home() {
+  const [view, setView] = useState("grid");
   return (
-    <div className="bg-white p-3 rounded-xl sm:ml-20 lg:ml-[276px]">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, rerum
-        commodi minima provident modi dignissimos necessitatibus laudantium
-        dicta nemo similique. Quidem quis est recusandae magni, dolorem unde sit
-        dicta reiciendis.
-      </p>
+    <div className="flex flex-col bg-white p-3 min-h-[calc(100vh-4rem)] rounded-xl sm:ml-20 lg:ml-[276px]">
+      <div className="flex justify-between items-center">
+        <h3 className="font-medium text-2xl">My Drive</h3>
+        <div className="flex border border-slate-400 rounded-3xl overflow-hidden">
+          <button
+            onClick={() => setView("grid")}
+            className={`flex justify-center items-center gap-1 w-12 sm:w-16 py-2 border-r border-slate-400 ${
+              view === "grid" ? "bg-[#c2e7ff]" : ""
+            }`}
+          >
+            {view === "grid" && (
+              <IoCheckmarkOutline size={18} className="hidden sm:block" />
+            )}
+            <FaList />
+          </button>
+          <button
+            onClick={() => setView("list")}
+            className={`flex justify-center items-center gap-1 w-12 sm:w-16 py-2 ${
+              view === "list" ? "bg-[#c2e7ff]" : ""
+            }`}
+          >
+            {view === "list" && (
+              <IoCheckmarkOutline size={18} className="hidden sm:block" />
+            )}
+            <FiGrid />
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col justify-center items-center gap-3 text-center">
+        <div className="w-full max-w-2xl">
+          <EmptyDrive />
+        </div>
+        <h3 className="font-medium text-2xl">No files in Drive</h3>
+        <span>Add files by uploading from your device</span>
+      </div>
     </div>
   );
 }
